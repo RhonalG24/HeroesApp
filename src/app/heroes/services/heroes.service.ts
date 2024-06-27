bca691d5d857488ce22080dee317a4e641d830bb
Rhonal Gonzalez <rgonzalez@originsw.com>
2024-06-27 14:48:03 -0300

    fin seccion 14

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments';
 
@@ -16,4 +16,14 @@ export class HeroesService {
     return this.http.get<Hero[]>(url);
   }
 
+  getHeroById(id:string): Observable<Hero|undefined> {
+    return this.http.get<Hero>(`${ this.baseUrl }/heroes/${ id }`)
+      .pipe(
+        catchError( error => of(undefined) )
+      );
+  }
+
+  getSuggestions(query:string):Observable<Hero[]>{
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes?superhero_like=${ query }&_limit=6`)
+  }
 }
